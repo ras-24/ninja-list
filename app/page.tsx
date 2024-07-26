309a3c3eab549cfe1888092e5e190b36324552e7
Ras <siregar.riyando@gmail.com>
2024-07-26 12:01:13 +0800

    Adding Image and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,31 @@
 import Link from "next/link";
 import styles from "@/styles/Home.module.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Ninja List | Home",
+  keywords: ["ninjas"],
+};
 
 export default function Home() {
   return (
     <div>
       <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet porro sequi natus quis cupiditate? Iste blanditiis autem praesentium aut asperiores? Nobis provident cum dicta, cupiditate accusantium dolore impedit animi quibusdam?</p>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet porro sequi natus quis cupiditate? Iste blanditiis autem praesentium aut asperiores? Nobis provident cum dicta, cupiditate accusantium dolore impedit animi quibusdam?</p>
-      <Link href="/ninjas" className={styles.btn}>See Ninja Listing</Link>
+      <p className={styles.text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet porro
+        sequi natus quis cupiditate? Iste blanditiis autem praesentium aut
+        asperiores? Nobis provident cum dicta, cupiditate accusantium dolore
+        impedit animi quibusdam?
+      </p>
+      <p className={styles.text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet porro
+        sequi natus quis cupiditate? Iste blanditiis autem praesentium aut
+        asperiores? Nobis provident cum dicta, cupiditate accusantium dolore
+        impedit animi quibusdam?
+      </p>
+      <Link href="/ninjas" className={styles.btn}>
+        See Ninja Listing
+      </Link>
     </div>
   );
 }
