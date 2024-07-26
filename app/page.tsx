6a680a0f21e0a89a5246b879be965b78960d0f9d
Ras <siregar.riyando@gmail.com>
2024-07-26 09:59:18 +0800

    Adding Linking Between Pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet porro sequi natus quis cupiditate? Iste blanditiis autem praesentium aut asperiores? Nobis provident cum dicta, cupiditate accusantium dolore impedit animi quibusdam?</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet porro sequi natus quis cupiditate? Iste blanditiis autem praesentium aut asperiores? Nobis provident cum dicta, cupiditate accusantium dolore impedit animi quibusdam?</p>
+      <Link href="/ninjas">See Ninja Listing</Link>
     </div>
   );
 }
