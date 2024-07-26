14663738a5e66c52feec80a0c5c6e591190a3d22
Ras <siregar.riyando@gmail.com>
2024-07-26 12:58:02 +0800

    Adding Fetching Data

--- app/ninjas/page.tsx
+++ app/ninjas/page.tsx
@@ -1,9 +1,34 @@
-const Ninjas = () => {
+import styles from "@/styles/Ninjas.module.css";
+
+interface Ninja {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  website: string;
+}
+
+async function getData(): Promise<Ninja[]> {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  return res.json();
+}
+
+export default async function Ninjas() {
+  const ninjas: Ninja[] = await getData();
+
   return (
     <div>
       <h1>All Ninjas</h1>
+      {ninjas.map(ninja => (
+        <div key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
+        </div>
+      ))}
     </div>
   );
 }
- 
-export default Ninjas;
