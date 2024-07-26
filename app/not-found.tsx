cc26d68891ff5866c37c33e16876e5bacc986a31
Ras <siregar.riyando@gmail.com>
2024-07-26 11:40:52 +0800

    Adding Redirecting Users

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,27 @@
+"use client";
+
 import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/');
+    }, 3000)
+  }, []);
+
   return (
     <div className="not-found">
       <h1>Oooops...</h1>
       <h2>That page cannot be found.</h2>
-      <p>Go back to the <Link href="/">Homepage</Link></p>
+      <p>
+        Go back to the <Link href="/">Homepage</Link>
+      </p>
     </div>
   );
-}
- 
+};
+
 export default NotFound;
