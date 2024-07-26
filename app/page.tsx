350eea9edd640d7c3d08021fab692c43bf04bd1e
Ras <siregar.riyando@gmail.com>
2024-07-26 11:04:35 +0800

    Adding styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
+import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet porro sequi natus quis cupiditate? Iste blanditiis autem praesentium aut asperiores? Nobis provident cum dicta, cupiditate accusantium dolore impedit animi quibusdam?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet porro sequi natus quis cupiditate? Iste blanditiis autem praesentium aut asperiores? Nobis provident cum dicta, cupiditate accusantium dolore impedit animi quibusdam?</p>
-      <Link href="/ninjas">See Ninja Listing</Link>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet porro sequi natus quis cupiditate? Iste blanditiis autem praesentium aut asperiores? Nobis provident cum dicta, cupiditate accusantium dolore impedit animi quibusdam?</p>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet porro sequi natus quis cupiditate? Iste blanditiis autem praesentium aut asperiores? Nobis provident cum dicta, cupiditate accusantium dolore impedit animi quibusdam?</p>
+      <Link href="/ninjas" className={styles.btn}>See Ninja Listing</Link>
     </div>
   );
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <div>
+    <footer>
       Copyright 2024 Ninja List
-    </div>
+    </footer>
   );
 }
  
