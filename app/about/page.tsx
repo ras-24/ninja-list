16aacce753f706073dbd323b0cdaddfd6b3bf8ee
Ras <siregar.riyando@gmail.com>
2024-07-26 09:45:53 +0800

    Adding Pages Routes

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit excepturi hic dolorum unde, eius optio molestias, expedita quas aut natus quis amet tempora itaque. Veniam nobis a quod quo quaerat.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit excepturi hic dolorum unde, eius optio molestias, expedita quas aut natus quis amet tempora itaque. Veniam nobis a quod quo quaerat.</p>
+    </div>
+  );
+}
+ 
+export default About;
